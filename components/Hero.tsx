fea2589c54092f9f03741e76eca09f9397e946c7
Vincent MEI <vincentmeipro@gmail.com>
2026-05-14 15:54:59 +0200

    Centre image hero sur le logo Elysium

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,7 @@ export default function Hero() {
         alt="Elysium Funded"
         fill
         priority
-        style={{ objectFit: "cover", objectPosition: "center top" }}
+        style={{ objectFit: "cover", objectPosition: "35% top" }}
       />
 
       {/* Gradient overlay pour lisibilité */}
