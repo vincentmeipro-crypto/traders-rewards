5965ebd7a3df37c71d767701ed4129c7d394e36d
Vincent MEI <vincentmeipro@gmail.com>
2026-05-15 18:05:03 +0200

    Fix mobile hero centering - force 100vw with translateX

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,10 +27,11 @@ export default function Hero() {
         {/* Image plein écran absolue */}
         <div style={{
           position: "absolute",
-          top: 0, left: 0, right: 0, bottom: 0,
+          top: 0, left: "50%", transform: "translateX(-50%)",
+          width: "100vw", height: "100%",
           backgroundImage: "url('/hero-mobile-new.png')",
           backgroundSize: "cover",
-          backgroundPosition: "center center",
+          backgroundPosition: "50% 50%",
           backgroundRepeat: "no-repeat",
         }} />
 
