3a80110e2981a0819a72a6f1c8a0bc7f6178f0df
Vincent MEI <vincentmeipro@gmail.com>
2026-05-17 14:40:59 +0200

    Hero: reduce gap between title and certified badge

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -164,7 +164,7 @@ export default function Hero() {
                 width: "auto",
                 verticalAlign: "top",
                 marginTop: "-0.05em",
-                marginLeft: "0.08em",
+                marginLeft: "0.02em",
                 mixBlendMode: "screen",
               }}
             />
