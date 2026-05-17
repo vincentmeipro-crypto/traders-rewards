408460d99ca9e79d86b140d2c2b1015dc4e42711
Vincent MEI <vincentmeipro@gmail.com>
2026-05-17 11:28:30 +0200

    Replace Crisp with Tawk.to live chat widget

--- components/CrispChat.tsx
+++ components/CrispChat.tsx
@@ -3,12 +3,13 @@ import { useEffect } from "react";
 
 export default function CrispChat() {
   useEffect(() => {
-    (window as any).$crisp = [];
-    (window as any).CRISP_WEBSITE_ID = "12bb26b9-91ca-4a8c-8b42-42a66d94b0f4";
-    const s = document.createElement("script");
-    s.src = "https://client.crisp.chat/l.js";
-    s.async = true;
-    document.head.appendChild(s);
+    const s1 = document.createElement("script");
+    const s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = "https://embed.tawk.to/6a0989e1c744531c437324e7/1joqk565d";
+    s1.charset = "UTF-8";
+    s1.setAttribute("crossorigin", "*");
+    s0.parentNode!.insertBefore(s1, s0);
   }, []);
 
   return null;
