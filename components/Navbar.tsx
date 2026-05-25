a6156d26d0efbbf8d87a38d730a084b694bf655d
Vincent MEI <vincentmeipro@gmail.com>
2026-05-25 16:37:22 +0200

    Fix French flag in navbar tagline using flagcdn img

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -49,8 +49,9 @@ export default function Navbar() {
               <span style={{ fontFamily: "var(--font-bebas)", fontSize: 26, letterSpacing: "3px", textTransform: "uppercase", lineHeight: 1, color: "#fff" }}>
                 Elysium
               </span>
-              <span style={{ fontSize: 10, fontWeight: 600, color: "#A0A0A0", letterSpacing: "1px", display: "flex", alignItems: "center", gap: 4 }}>
-                🇫🇷 PropFirm Française
+              <span style={{ fontSize: 10, fontWeight: 600, color: "#A0A0A0", letterSpacing: "1px", display: "flex", alignItems: "center", gap: 5 }}>
+                <img src="https://flagcdn.com/20x15/fr.png" width={14} height={11} alt="FR" style={{ borderRadius: 1, objectFit: "cover" }} />
+                PropFirm Française
               </span>
             </div>
           </a>
