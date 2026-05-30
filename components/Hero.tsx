acaf48a64902b8ae5ae91e04d8636187630fe639
Vincent MEI <vincentmeipro@gmail.com>
2026-05-30 09:08:38 +0200

    Hero logo +50% size

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -327,7 +327,7 @@ export default function Hero() {
         <img
           src="/logo-elysium-rewards.png"
           alt="Elysium Rewards"
-          style={{ height: isMobile ? 58 : 77, width: "auto", objectFit: "contain", marginBottom: 24 }}
+          style={{ height: isMobile ? 87 : 116, width: "auto", objectFit: "contain", marginBottom: 24 }}
         />
 
         {/* Title */}
